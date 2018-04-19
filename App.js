265387d4576be540a08075b49edeaecdfe5d3a4c
Ryuji Eguchi <jp.ryuji@gmail.com>
2018-04-19 15:45:19 +0900

    Update App.js

--- App.js
+++ App.js
@@ -1,58 +1,220 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
-  Platform,
+  AppRegistry,
+  Button,
+  ScrollView,
   StyleSheet,
   Text,
   View
 } from 'react-native';
+import {
+  StackNavigator,
+  DrawerNavigator,
+  DrawerItems
+} from 'react-navigation';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+///////////////////////////////////////////////////////////////////////
+//
+// Styles
+//
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
+const styles = StyleSheet.create({
+  view1: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  text1:{
+    fontSize: 26,
+  },
+  icon1:{
+    marginLeft: 16,
+  },
+  headerHome: {
+    backgroundColor: '#CDDC39',
+  },
+  headerGame: {
+    backgroundColor: '#4CAF50',
+  },
+  headerBook: {
+    backgroundColor: '#FFC107',
+  },
+})
+
+///////////////////////////////////////////////////////////////////////
+//
+// HOME
+//
+
+class HomeScreen extends React.Component {
+
+  static navigationOptions = ({navigation}) => ({
+    title:'Home',
+    drawerIcon: <Icon name="home" size={24} color="#4CAF50"/>,
+    headerStyle: styles.headerHome,
+    headerLeft: (
+      <Icon name="bars" size={24}
+        style={styles.icon1}
+        onPress={()=>{navigation.navigate('DrawerOpen')}} />
+    ),
+  });
+
+  render(){
+    const { navigate } = this.props.navigation;
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+      <View style={styles.view1}>
+        <Text style={styles.text1}>Home</Text>
+        <Button title="Next" onPress={()=>navigate('HomeNext')}/>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+///////////////////////////////////////////////////////////////////////
+//
+// HOME > Next
+//
+
+class HomeNextScreen extends React.Component {
+  static navigationOptions = {
+    title:'Home Next',
+    headerStyle: styles.headerHome,
+  };
+
+  render() {
+    return (
+      <View style={styles.view1}>
+        <Text style={styles.text1}>Next</Text>
+      </View>
+    );
+  }
+}
+
+
+///////////////////////////////////////////////////////////////////////
+//
+// GAME
+//
+
+class GameHomeScreen extends React.Component {
+
+  static navigationOptions = ({navigation}) => ({
+    title:'Games',
+    drawerIcon: <Icon name="gamepad" size={24} color="#FF5722"/>,
+    headerStyle: styles.headerGame,
+    headerLeft: (
+      <Icon name="bars" size={24}
+        style={styles.icon1}
+        onPress={()=>{navigation.navigate('DrawerOpen')}} />
+    ),
+  });
+
+  render(){
+    const { navigate } = this.props.navigation;
+    return (
+      <View style={styles.view1}>
+        <Text style={styles.text1}>Game Home</Text>
+        <Button title="Start ABC" onPress={()=>navigate('GameABC')}/>
+      </View>
+    );
+  }
+}
+
+///////////////////////////////////////////////////////////////////////
+//
+// GAME > GAME ABC
+//
+
+class GameABCScreen extends React.Component {
+
+  static navigationOptions = ({navigation}) => ({
+    title:'Game ABC',
+    headerStyle: styles.headerGame,
+  });
+
+  render(){
+    return (
+      <View style={styles.view1}>
+        <Text style={styles.text1}>Game ABC</Text>
+      </View>
+    );
+  }
+}
+
+
+///////////////////////////////////////////////////////////////////////
+//
+// BOOKS
+//
+
+class BookHomeScreen extends React.Component {
+  static navigationOptions = ({navigation}) => ({
+    title:'Books',
+    drawerLabel: 'Books - Drawer Label',
+    drawerIcon: <Icon name="book" size={24} color="#795548"/>,
+    headerStyle: styles.headerBook,
+    headerLeft: (
+      <Icon name="bars" size={24}
+        style={styles.icon1}
+        onPress={()=>{navigation.navigate('DrawerOpen')}} />
+    ),
+  });
+
+  render(){
+    return (
+      <View style={styles.view1}>
+        <Text style={styles.text1}>Books</Text>
+      </View>
+    );
+  }
+}
+
+///////////////////////////////////////////////////////////////////////
+//
+// Define Stack Structure
+//
+
+const HomeStack = StackNavigator({
+  Home: { screen: HomeScreen, },
+  HomeNext: { screen: HomeNextScreen, },
 });
+
+const GameStack = StackNavigator({
+  GameHome: { screen: GameHomeScreen, },
+  GameABC: { screen: GameABCScreen, }
+});
+
+const BookStack = StackNavigator({
+  BookHome: { screen: BookHomeScreen, }
+});
+
+///////////////////////////////////////////////////////////////////////
+//
+// Drawer
+//
+
+const MyApp = DrawerNavigator({
+    HomeStack: { screen: HomeStack, },
+    GameStack: { screen: GameStack, },
+    BookStack: { screen: BookStack, }
+  },
+  {
+    drawerWidth: 300,
+    contentComponent: props => (
+      <ScrollView>
+        <View style={{padding:16,}}>
+          <Text style={{fontSize:24}}>DRAWER TEST</Text>
+        </View>
+        <DrawerItems {...props} />
+      </ScrollView>
+    ),
+  }
+);
+
+///////////////////////////////////////////////////////////////////////
+//
+// export
+//
+
+export default MyApp;
